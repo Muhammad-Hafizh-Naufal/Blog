5a60e285df4c3399535bd4aa213d5b8e809d9eb7
@Hafizh_Naufal <noval1122567@gmail.com>
2024-09-25 17:37:22 +0700

    stuck Membuat Fungsi untuk Memanggil Single Data Post

---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+
+  setup(props) {
+    return {};
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/PostSingle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'ShowPost', params: { id: post.id } }">
+      <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: ["post"],
+
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + " ...";
+    });
+
+    return { snippet };
+  },
+};
+</script>
+
+<style></style>
